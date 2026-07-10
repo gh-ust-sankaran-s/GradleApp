6b04589eae3204d499d29ce9dc6e0bee11ec37db
Sankaran Selvaraj(UST <304557@ust.com>
2026-07-10 11:57:56 +0530

    Passing all the test

--- src/test/java/postgreSqlCon/OrderTest.java
+++ src/test/java/postgreSqlCon/OrderTest.java
@@ -2,7 +2,7 @@ package postgreSqlCon;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
 
@@ -12,7 +12,7 @@ class OrderTest {
                 .withQuantity(3)
                 .build();
 
-        assertEquals(4, order.getQuantity());
+        assertEquals(3, order.getQuantity());
     }
 
     @Test
@@ -21,5 +21,68 @@ class OrderTest {
 
         assertEquals(1, order.getQuantity());
         assertEquals("NEW", order.getStatus());
+    }
+
+    @Test
+    void shouldSetSkuUsingBuilder() {
+        Order order = OrderBuilder.anOrder()
+                .withSku("SKU-100")
+                .build();
+
+        assertEquals("SKU-100", order.getSku());
+    }
+
+    @Test
+    void shouldCreateOrderWithCustomStatus() {
+        Order order = OrderBuilder.anOrder()
+                .withStatus("COMPLETED")
+                .build();
+
+        assertEquals("COMPLETED", order.getStatus());
+    }
+
+    @Test
+    void shouldMarkOrderAsRefunded() {
+        Order order = OrderBuilder.anOrder()
+                .withRefunded(true)
+                .build();
+
+        assertTrue(order.isRefunded());
+    }
+
+    @Test
+    void shouldCreateOrderWithQuantityFromFactory() {
+        Order order = OrderFactory.anOrderWithQuantity(5);
+
+        assertEquals(5, order.getQuantity());
+    }
+
+    @Test
+    void shouldBeEqualForSameValues() {
+        Order order1 = OrderBuilder.anOrder()
+                .withSku("SKU-1")
+                .withQuantity(2)
+                .build();
+
+        Order order2 = OrderBuilder.anOrder()
+                .withSku("SKU-1")
+                .withQuantity(2)
+                .build();
+
+        assertEquals(order1, order2);
+        assertEquals(order1.hashCode(), order2.hashCode());
+    }
+
+    @Test
+    void shouldNotBeEqualForDifferentValues() {
+        Order order1 = OrderBuilder.anOrder()
+                .withQuantity(1)
+                .build();
+
+        Order order2 = OrderBuilder.anOrder()
+                .withQuantity(2)
+                .build();
+
+        assertNotEquals(order1, order2);
     }
 }
